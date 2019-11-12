ac49373898312e04833af756f8cddc1230f25195
pzxbc <pzxbc@qq.com>
2019-11-12 19:22:29 +0800

    add navigation

--- android/app/src/main/java/com/rntutorial/MainApplication.java
+++ android/app/src/main/java/com/rntutorial/MainApplication.java
@@ -3,6 +3,10 @@ package com.rntutorial;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+import com.swmansion.rnscreens.RNScreensPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new SafeAreaContextPackage(),
+            new RNScreensPackage(),
+            new RNGestureHandlerPackage(),
+            new ReanimatedPackage()
       );
     }
 
---
+++ src/screens/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import SafeAreaView from 'react-native-safe-area-view'
+
+class Home extends React.Component {
+  static navigationOptions = ({navigation, navigationOptions}) => {
+    return {
+      header: null
+    }
+  }
+  render () {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>HomeTest</Text>
+        <Button 
+          title="FontTutorial"
+          color="#841584"
+          onPress={() => this.props.navigation.navigate('FontTutorial')}
+        />
+      </SafeAreaView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  }
+})
+
+export default Home
