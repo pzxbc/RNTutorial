5ba148b81a7155f8d566d3d998c852a379617133
gzpengzhangxiang <gzpengzhangxiang@corp.netease.com>
2019-11-13 09:50:24 +0800

    react navigation 依赖的gesture库安装成功

--- android/app/src/main/java/com/rntutorial/MainApplication.java
+++ android/app/src/main/java/com/rntutorial/MainApplication.java
@@ -3,6 +3,7 @@ package com.rntutorial;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 
 
 
@@ -25,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNGestureHandlerPackage()
       );
     }
 
